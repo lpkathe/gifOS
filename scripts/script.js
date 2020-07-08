ce438ba90955e1e6ecb67b4a91903e6a9f458236
Katerine Martinez <lp.kathe@gmail.com>
2020-07-08 17:37:07 -0500

    Dark mode - html

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,11 @@
-function trending(params) {
-    fetch()
-    .then(res => {
-        console.log(res)
-    })
-    
+function search(text) {
+    let answer = getElementById("searchInput");
+    console.log(answer);
 }
+
+
+/*const key = RdoBL837xzyR4wgjoqf8FocqUIoxGh0q;
+
+const endPointSearch = 'https://api.giphy.com/v1/gifs/search?api_key=RdoBL837xzyR4wgjoqf8FocqUIoxGh0q&q=&limit=25&offset=0&rating=g&lang=en';
+
+fetch(endPointSearch).then(answer=>)*/
