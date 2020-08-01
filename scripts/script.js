4aab3fa3e5e3ae5f6e85f02d142ebdad4ec46deb
Katerine Martinez <lp.kathe@gmail.com>
2020-08-01 15:08:43 -0500

    Search - btn js

--- scripts/script.js
+++ scripts/script.js
@@ -1,48 +1,56 @@
-import GiphyApi from './GiphyApi.js'
+import GiphyApi from './GiphyApi.js';
 
 /**
  * Global variables
  */
 const inputSearch = document.getElementById("inputSearch");
 const iconSearch = document.getElementById("iconSearch");
-const resultsCards = document.querySelector(".results__cards");
+const suggestedList = document.querySelector(".search__form__list");
 const pTrendingCategories = document.querySelector(".search__p");
-const suggestedSearch = document.getElementById("suggestedSearch");
+const resultsContainer = document.querySelector("results__container");
+const resultsTitle = document.querySelector(".results__title");
+const resultsCards = document.querySelector(".results__cards");
 
 /**
  * Functions that load with the page.
  */
 function onLoad() {
     getTrendingCategories();
-}
+};
 
 /**
  * Get gifs of the search results and display then on html
  */
-function search() {
+function search(pagination) {
     const { search } = GiphyApi;
-    
+
+    resultsTitle.innerText = capitalize(inputSearch.value);
+    resultsCards.innerHTML = "";
+
     search(inputSearch.value)
-    .then((response) => {
-        response.data.forEach((element) => {
-            createImage(element.images.original.url);
+        .then((response) => {
+            response.data.forEach((element) => {
+                createCard(element.images.original.url);
+            })
+        }).catch ((error) => {
+            resultsCards.innerText = "Error "+ error;
         })
-    }) .catch ((error) => {
-        resultsContainer.innerText = "Error "+ error;
-    });
-}
+    
+    btnSearchResults(pagination);
+};
 
 /**
  * Create cards for results
- * @param {} url 
- * @param {*} title 
+ * @param {string} url 
+ * @param {string} title 
  */
-function createImage(url, title) {
-    const image = document.createElement("img");
+function createCard(url) {
+    const card = document.createElement("img");
     
-    resultsCards.appendChild(image);
+    card.src = url;
+    card.className = "results__card";
     
-    image.src = url;
+    resultsCards.appendChild(card);
 };
 
 /**
@@ -55,7 +63,7 @@ function getTrendingCategories() {
     trendingCategories()
     .then((response) => {
         response.data.forEach((element) => {
-            list.push(element.name);
+            list.push(capitalize(element.name));
         });
         pTrendingCategories.innerText = list.join(', ');
     }) .catch ((error) => {
@@ -66,25 +74,65 @@ function getTrendingCategories() {
 /**
  * Suggestion search
  */
-function getAutocompleteSearch() {
+function getAutocompleteSearch(event) {
     const { autocompleteSearch } = GiphyApi;
 
-    suggestedSearch.innerText = "";
+    suggestedList.innerText = "";
 
     autocompleteSearch(inputSearch.value)
-    .then((response) => {
-        response.data.forEach((element) => {
-            const option = document.createElement('option');
-            suggestedSearch.appendChild(option);
-            option.value = element.name;
+        .then((response) => {
+            response.data.forEach((element) => {
+                const li = document.createElement('li');
+                li.value = element.name;
+                li.className = "search__form__li";
+                suggestedList.appendChild(li);
+            });
+        }).catch ((error) => {
+            console.log(error);
         });
-    });
+    
+    if (event.keyCode == 13) {
+        search();
+    }
+};
+
+/**
+ * make first letter of a string uppercase
+ * @param {string} text 
+ */
+function capitalize(text) {
+    if (text.length <= 0 ) {
+        return "";
+    }
+
+    return text.charAt(0).toUpperCase() + text.slice(1); 
+};
+
+/**
+ * Create button for more results
+ * @param {number} pagination 
+ */
+function btnSearchResults(pagination) {
+    const btnVerMas = create.Element("button");
+
+    if (pagination.element = pagination[0]){
+        resultsContainer.appendChild(btnVerMas);
+        btnVerMas.value = "VER MÁS";
+        btnVerMas.className = "results__button";
+    }
+
+    if (pagination.element = pagination.length) {
+        resultsContainer.removeChild(button);
+    } else {
+        return pagination;
+    }
 };
 
 /**
  * Events
  */
 
+window.addEventListener("load", onLoad);
 inputSearch.addEventListener("keypress", getAutocompleteSearch);
 iconSearch.addEventListener("click", search);
-window.addEventListener("load", onLoad);
+btnVerMas.addEventListener("click", search);
