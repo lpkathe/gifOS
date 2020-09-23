2fd52b64e955397e8aa8d1b3c25a159e4b3dbb14
Katerine Martinez <lp.kathe@gmail.com>
2020-09-23 17:45:16 -0500

    CORS fix

--- scripts/GiphyApi.js
+++ scripts/GiphyApi.js
@@ -1,4 +1,4 @@
-const DOMAIN = `://api.giphy.com/v1/gifs/`;
+const DOMAIN = `https://api.giphy.com/v1/gifs/`;
 const urlSearch = DOMAIN + "search";
 const urlAutocompleteSearch = urlSearch + "/tags";
 const urlTrendingCategories = DOMAIN + "categories";
